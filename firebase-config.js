f9d9b75c120d13110623c06737b497b3b2b33565
araskapaska <paveltukhtin3@gmail.com>
2025-05-03 22:25:52 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCha0f3X7fcU44F3GnNluEcdvHSKV9kbcA",
+  authDomain: "hiio-80577.firebaseapp.com",
+  databaseURL: "https://hiio-80577-default-rtdb.firebaseio.com",
+  projectId: "hiio-80577",
+  storageBucket: "hiio-80577.firebasestorage.app",
+  messagingSenderId: "837168042630",
+  appId: "1:837168042630:web:dd73b45a995f768d2e3707",
+  measurementId: "G-QDCHPCLMQM"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
+
+export { auth, db };
